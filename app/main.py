5a548e843159f7f145dc22839a9b1516a59e4f02
Alex Nicol <xelanicol@gmail.com>
2024-06-03 16:55:53 +0800

    Commented out the raw SQL path operations / routes and replaced with ORM version (python in place of SQL) using SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -19,7 +19,6 @@ app = FastAPI()
 
 # from FASTAPI Documentation:
 
-
 class Post(BaseModel):
     title: str # validation 
     content: str # validation
@@ -61,71 +60,108 @@ def root(): # previously had 'async' before def, removed because optional
 
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
-    return {"status":"success"}
-
-# GET path operation
-# go to root/posts to get this!
-@app.get("/posts") # if we put only "/" then it will go to the first method in the script with this path (root())
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
+    posts = db.query(models.Post).all() # Note how use of ORM (sqlalchemy) means no SQL code here!
+    # equivalent of 'SELECT * FROM posts'
     return {"data":posts}
 
-# POST path operation
-@app.post("/createposts", status_code=status.HTTP_201_CREATED) # HTTP POST method. NB. not best practice to do this
-def create_posts(post: Post): #FastAPI will validate the input according to 'Post' class
-    # use of the %s format prevents 'SQL INJECTION' attacks
-    cursor.execute("""INSERT INTO posts (title,content,published) VALUES (%s, %s, %s) RETURNING * """,
-                   vars=(post.title,post.content,post.published))
-    new_post = cursor.fetchone() # get input back
-    conn.commit() # commit changes to SQL database
+# GET path operation (using raw SQL)
+# go to root/posts to get this!
+# @app.get("/posts") # if we put only "/" then it will go to the first method in the script with this path (root())
+# def get_posts():
+#     cursor.execute("""SELECT * FROM posts""")
+#     posts = cursor.fetchall()
+#     return {"data":posts}
+
+# GET path operation (using ORM: sqlalchemy)
+@app.get("/posts") # if we put only "/" then it will go to the first method in the script with this path (root())
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return {"data":posts}
+
+# POST path operation (raw SQL)
+# @app.post("/createposts", status_code=status.HTTP_201_CREATED) # HTTP POST method. NB. not best practice to do this
+# def create_posts(post: Post): #FastAPI will validate the input according to 'Post' class
+#     # use of the %s format prevents 'SQL INJECTION' attacks
+#     cursor.execute("""INSERT INTO posts (title,content,published) VALUES (%s, %s, %s) RETURNING * """,
+#                    vars=(post.title,post.content,post.published))
+#     new_post = cursor.fetchone() # get input back
+#     conn.commit() # commit changes to SQL database
+#     return {"data":new_post}
+
+# POST path operation (ORM: sqlalchemy)
+@app.post("/createposts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.model_dump()) # unpack object-converted-to-dict to get all fields from object
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post) # retrieve new post (after defaults added)
     return {"data":new_post}
 
-# GET/ID path operation
+# GET/ID path operation (raw SQL)
+# @app.get("/posts/{id}")
+# def get_post(id: str):
+#     cursor.execute("""SELECT * FROM posts WHERE id = %s """,vars=(str(id),)) # must convert to str
+#     # comma in vars tuple seems necessary to prevent 500 error
+#     post = cursor.fetchone()
+#     print(post)
+#     if not post:
+#         # this line does the action of the following 2 lines
+#         raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+#         # response.status_code = status.HTTP_404_NOT_FOUND
+#         # return {'message': f"post with id: {id} was not found"}
+#     return {'post_detail':post}
+
+# GET/ID path operation (ORM: sqlalchemy)
 @app.get("/posts/{id}")
-def get_post(id: str):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s """,vars=(str(id),)) # must convert to str
-    # comma in vars tuple seems necessary to prevent 500 error
-    post = cursor.fetchone()
-    print(post)
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first() # need .all() or .first() to actually run SQL
+    # we know there is just 1, so use .first() (because ID is unique)
     if not post:
-        # this line does the action of the following 2 lines
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {'message': f"post with id: {id} was not found"}
-    return {'post_detail':post}
-    
+    return {"data":post}
 
-@app.get("/posts/latest") # DOES NOT WORK, because it thinks it is using previous function, expecting int input
-# if we moved before previous function it would work, but better to use different name
-def get_latest_post():
-    return my_posts[-1] # return newest post
+# DELETE path operation (raw SQL)
+# @app.delete("/posts/{id}")
+# def delete_post(id: int):
+#     cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+#     deleted_post = cursor.fetchone()
+#     conn.commit()
+#     if deleted_post is None:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+#                             detail=f"post with id: {id} does not exist")
+#     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-# DELETE path operation
+# DELETE path operation (ORM: sqlalchemy)
 @app.delete("/posts/{id}")
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-    if deleted_post is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id: {id} does not exist")
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if not post.first(): #if post doesn't exist
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    deleted_post = post.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
             
 
-# UPDATE path operation
+# UPDATE path operation (raw SQL)
+# @app.put("/posts/{id}")
+# def update_post(id: int, post: Post):
+#     cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
+#                    (post.title, post.content, post.published, str(id)))
+#     updated_post = cursor.fetchone()
+#     conn.commit()
+#     if updated_post is None:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+#                             detail=f"post with id: {id} does not exist")
+#     return {"data":updated_post}
+
+# UPDATE path operation (ORM: sqlalchemy)
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
-                   (post.title, post.content, post.published, str(id)))
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if updated_post is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id: {id} does not exist")
-    return {"data":updated_post}
-
-
-
+def update_post(id: int, post: Post, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    if not post_query.first(): #if post doesn't exist
+            raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    post_query.update(post.model_dump(), synchronize_session=False)
+    db.commit()
+    return {"data":post_query.first()}
 
 
--- app/models.py
+++ app/models.py
@@ -1,9 +1,14 @@
 from .database import Base
+from sqlalchemy.sql.expression import text
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+
+# sqlalchemy won't change attributes of an existing table
 
 class Post(Base):
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, nullable=True, default=True)
+    published = Column(Boolean, nullable=False, server_default='TRUE') # must be server_default not default
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default = text('now()'))
